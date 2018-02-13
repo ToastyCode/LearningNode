74d426a947ea8b05d1f22f8e2e077598fdeed7e6
ToastyCode <animx001@gmail.com>
2018-02-13 15:51:18 +0100

    first Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html' : 'text/html',
+    '.css' : 'text/css'
+};
+
+serverRequestListener = function(request,response){
+    var lookup = path.basename(decodeURI(request.url)) || 'index.html'
+    var f = 'content/' + lookup;
+    fs.exists(f, function(exists){
+        console.log(exists? lookup + " is there" : lookup + " doesn't exist");
+    });
+}
+
+var server = http.createServer(serverRequestListener);
+
+server.listen(8080);
